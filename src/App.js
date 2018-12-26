578da0d38efa4278cac5ba9644432be0cd22753a
Shivansh Nalwaya <shivanshnalwaya@gmail.com>
2018-12-26 19:39:52 +0530

    Remove react router to work with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./routes/Home";
-// import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
-
-const AppRouter = () => (
-  <Router>
-    <div>
-      <Route path="/" exact component={Home} />
-      <Route path="/about/" component={About} />
-      <Route path="/users/" component={Users} />
-    </div>
-  </Router>
-);
+const AppRouter = () => <Home />;
 
 export default AppRouter;
